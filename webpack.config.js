b87a2fb7bd8aa71ca6578942b0bdc7eabb0252f7
Ersen SARI <ersensari@outlook.com>
2018-12-17 22:58:37 +0300

    package.json

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = () => {
       }
     },
     output: {
-      path: path.join(__dirname, bundleOutputDir),
+      path: path.join(__dirname, bundleOutputDir), 
       filename: '[name].js',
       publicPath: '/dist/'
     },
